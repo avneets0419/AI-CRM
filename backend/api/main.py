1e8fe877e9eec130303db3f09100cb6370584d71
avneets0419 <avneet0419@gmail.com>
2026-04-11 19:45:00 +0530

    deploy: Fixed CORSError

--- backend/api/main.py
+++ backend/api/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Krazimo Mini AI CRM", version="2.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://ai-crm-lake-nine.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
